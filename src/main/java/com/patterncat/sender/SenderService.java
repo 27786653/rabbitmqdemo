bd7fd9a18d6a01e2108df7b16296297c55f000ac
patterncat <patterncat@gmail.com>
2016-02-02 02:36:24 +0800

    init

---
+++ src/main/java/com/patterncat/sender/SenderService.java
@@ -0,0 +1,25 @@
+package com.patterncat.sender;
+
+import com.patterncat.model.Bar;
+import com.patterncat.model.Foo;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by patterncat on 2016-02-02.
+ */
+@Component
+public class SenderService {
+
+    @Autowired
+    private RabbitMessagingTemplate rabbitMessagingTemplate;
+
+    public void sendFoo2Rabbitmq(final Foo foo) {
+        this.rabbitMessagingTemplate.convertAndSend("exchange", "queue.foo", foo);
+    }
+
+    public void sendBar2Rabbitmq(final Bar bar){
+        this.rabbitMessagingTemplate.convertAndSend("exchange", "queue.bar", bar);
+    }
+}
